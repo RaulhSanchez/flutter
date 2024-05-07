29d976705a0459d692a70591c0e8e2b389517760
RaulHSanchez <92sanchez.raul@gmail.com>
2024-05-07 20:12:13 +0200

    Inicio config

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,9 +5,33 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('hola, Screen'),
+    const fontSize30 = TextStyle(fontSize: 30);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('HomeScreen'),
+        elevation: 10,
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Numero de Clicks',
+              style: fontSize30,
+            ),
+            Text(
+              '3',
+              style: fontSize30,
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print('object');
+        },
       ),
     );
   }
